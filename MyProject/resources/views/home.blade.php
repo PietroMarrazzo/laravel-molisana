070ef8b7548c0fe907a030f3838905e9c6538809
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-12-22 15:41:21 +0100

    job done

--- MyProject/resources/views/home.blade.php
+++ MyProject/resources/views/home.blade.php
@@ -8,7 +8,50 @@
 
 
 {{-- section products --}}
+<section class="pasta-type container">
+    <h2>LE LUNGHE</h2>
+    <ul class="cards">
+        @foreach ($lunghe as $card)
+            <li class="card">
+                <a href="">
+                    <img src="{{ $card['src'] }}" alt="product img">
+                    <h3>{{ $card['titolo'] }}</h3>
+                    <h4>Vedi prodotto</h4>
+                </a>                
+            </li>   
+        @endforeach
+    </ul>
+</section>
 
+<section class="pasta-type container">
+    <h2>LE CORTE</h2>
+    <ul class="cards">
+        @foreach ($corte as $card)
+            <li class="card">
+                <a href="">
+                    <img src="{{ $card['src'] }}" alt="product img">
+                    <h3>{{ $card['titolo'] }}</h3>
+                    <h4>Vedi prodotto</h4>
+                </a>                
+            </li>   
+        @endforeach
+    </ul>
+</section>
+
+<section class="pasta-type container">
+    <h2>LE CORTISSIME</h2>
+    <ul class="cards">
+        @foreach ($cortissime as $card)
+            <li class="card">
+                <a href="">
+                    <img src="{{ $card['src'] }}" alt="product img">
+                    <h3>{{ $card['titolo'] }}</h3>
+                    <h4>Vedi prodotto</h4>
+                </a>                
+            </li>   
+        @endforeach
+    </ul>
+</section>
 
 
 
